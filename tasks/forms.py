9603ee174fd0dc17bef49f5f8d8d2384f63072f0
Alejandro Rivera <jalejandro142@gmail.com>
2024-12-20 23:52:52 -0500

    Se finaliza el proceso de colocar estilos a los templates requeridos

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,9 +1,14 @@
 
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
 
-class Task_form(ModelForm):
+class Task_form(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Asigna titulo de tarea'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Describe tu tarea'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input '}),
+        }
